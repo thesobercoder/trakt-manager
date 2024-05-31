4516fa84a5469f7e76d848069a7c8a5450bbe39d
Soham Dasgupta <soham@thesobercoder.in>
2024-05-31 16:06:36 +0530

    fix: use path.join

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -2,9 +2,10 @@ import { environment } from "@raycast/api";
 import { readFile, writeFile } from "fs/promises";
 import { existsSync } from "node:fs";
 import { unlink } from "node:fs/promises";
+import path from "path";
 
 export const setFileCache = async (key: string, content: string) => {
-  const filePath = `${environment.supportPath}/${key}.json`;
+  const filePath = path.join(`${environment.supportPath}`, `${key}.json`);
   if (existsSync(filePath)) {
     await unlink(filePath);
   }
@@ -12,7 +13,7 @@ export const setFileCache = async (key: string, content: string) => {
 };
 
 export const getFileCache = async (key: string) => {
-  const filePath = `${environment.supportPath}/${key}.json`;
+  const filePath = path.join(`${environment.supportPath}`, `${key}.json`);
   if (!existsSync(filePath)) {
     return undefined;
   }
