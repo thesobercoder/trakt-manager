c2759cb6b6ce6249ece1907ff0de0563003355dc
Soham Dasgupta <soham@thesobercoder.in>
2024-06-19 01:36:53 +0530

    fix: unnecessary rerender bug

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -1,15 +1,15 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useMemo, useState } from "react";
 import { authorize } from "../lib/oauth";
 
 export const View = ({ children }: { children: ReactNode }) => {
-  const [x, forceRerender] = useState(0);
+  const [, forceRerender] = useState(0);
 
-  useEffect(() => {
+  useMemo(() => {
     (async () => {
       await authorize();
       forceRerender((value) => value + 1);
     })();
-  }, [x]);
+  }, []);
 
   return children;
 };
