b7f55a82c52b0042296014e1fb921d739dc96a88
Soham Dasgupta <soham@thesobercoder.in>
2024-06-19 00:59:25 +0530

    fix: refresh command crash if user not authorized

--- src/refresh.tsx
+++ src/refresh.tsx
@@ -1,7 +1,9 @@
 import { LocalStorage } from "@raycast/api";
+import { authorize } from "./lib/oauth";
 import { getUpNextShows } from "./services/shows";
 
 export default async function Command() {
+  await authorize();
   await LocalStorage.removeItem("upNextShows");
   await getUpNextShows();
 }
